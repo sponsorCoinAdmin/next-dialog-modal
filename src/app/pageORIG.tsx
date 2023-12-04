0527184ac5ca0e0ba0827a376118c381c4047286
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2023-12-04 18:36:52 -0500

    WIP

---
+++ src/app/pageORIG.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link"
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-5xl">Home</h1>
+
+      <Link href="/products?showDialog=y" className="text-3xl underline">Go to Products with Modal</Link>
+
+      <Link href="/products" className="text-3xl underline">Go to Products without Modal</Link>
+
+      {/* <div className="ModalButton">
+        <button
+          className="tokenList-modal-button2"
+
+          onClick={() => {
+            alert("BUTTON SELECTED");
+          }}
+        >
+          OpenDialogueModal
+        </button> */}
+      </div>
+    </>
+  )
+}
